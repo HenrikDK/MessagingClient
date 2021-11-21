d0ecdbbd2441f3f57acb8da982ac35446c15eeac
HenrikDK <commander.john.crichton@gmail.com>
2021-11-21 19:48:43 +0100

    replace abstract class with interface that has an internal default implementation

---
+++ src/Messaging.Client/IMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Messaging.Client
+{
+    public interface IMessageHandler<T>
+    {
+        public void Handle(T message, Guid messageId);
+
+        internal void Handle(object value, Guid messageId)
+        {
+            Handle((T) value, messageId);
+        }
+    }
+}
