e69f061054dbf8699bccfa7ca9118bb8e5fe0d11
HenrikDK <commander.john.crichton@gmail.com>
2021-11-21 19:50:10 +0100

    update test handler implementation to what you would expect

--- src/Messaging.Client.Test/TestHandler.cs
+++ src/Messaging.Client.Test/TestHandler.cs
@@ -2,9 +2,13 @@
 
 namespace Messaging.Client.Test
 {
-    class TestHandler : MessageHandler<TestMessage>
+    public interface ITestHandler : IMessageHandler<TestMessage>
     {
-        public override void Handle(TestMessage message, Guid messageId)
+    }
+    
+    class TestHandler : ITestHandler
+    {
+        public void Handle(TestMessage message, Guid messageId)
         {
             Console.WriteLine($"test message {message.MyDate}");
         }
