0fe1e5a3fcb02414223258ac18dc7e0219387f03
HenrikDK <commander.john.crichton@gmail.com>
2021-11-21 23:57:05 +0100

    add global usings

---
+++ src/Messaging.Client/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using System;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Reflection;
+global using System.Text;
+global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Logging;
+
+global using Azure.Messaging.EventHubs;
+global using Azure.Messaging.EventHubs.Processor;
+global using Azure.Messaging.EventHubs.Producer;
+global using Azure.Storage.Blobs;
