1563de6ad0b95d467dbecb0f79d02b8adf7bb21c
HenrikDK <commander.john.crichton@gmail.com>
2021-11-21 02:52:45 +0100

    work on producer

--- src/Messaging.Client/MessageProducer.cs
+++ src/Messaging.Client/MessageProducer.cs
@@ -1,5 +1,35 @@
-﻿namespace Messaging.Client;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace Messaging.Client;
 
 public class MessageProducer
 {
+    private readonly IConfiguration _configuration;
+    private EventHubProducerClient _producer;
+    
+    public MessageProducer(IConfiguration configuration, string topic)
+    {
+        _configuration = configuration;
+        _producer = new EventHubProducerClient(_configuration.GetValue<string>("event-hub"), topic);
+    }
+    
+    public void SendMessages(IList<object> messages)
+    {
+        // TODO handle batching.
+        var enveloped = messages.Select(x => new EventData(JsonSerializer.Serialize(x))
+            {
+                MessageId = Guid.NewGuid().ToString(),
+                Properties = { new KeyValuePair<string, object>("MessageName", x.GetType().Name ) },
+                ContentType = "json"
+            }
+        );
+
+        _producer.SendAsync(enveloped).Wait();
+    }
 }
