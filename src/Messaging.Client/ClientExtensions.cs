07c84c0b8b4a06369aa243f41eb53a8a03b80cd0
HenrikDK <commander.john.crichton@gmail.com>
2021-11-21 01:50:49 +0100

    add extensions

---
+++ src/Messaging.Client/ClientExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Messaging.Client;
+
+public static class ClientExtensions
+{
+    /// <summary>
+    /// Internal method used to determine the type of the handlers parameter for deserialization purposes
+    /// </summary>
+    /// <param name="handler">The handler who's type has to be determined</param>
+    /// <returns>Type of message the handler accepts</returns>
+    public static Type GetHandlerType<T>(this IMessageHandler<T> handler) where T : class, IMessage
+    {
+        return typeof(T);
+    }
+    
+    /// <summary>
+    /// register a handler with the consumer, uses reflection to determine message name if none is provided. 
+    /// </summary>
+    /// <param name="consumer">The event consumer that will process the messages</param>
+    /// <param name="handler">An instance of IMessageHandler that will handle the specific message type.</param>
+    /// <param name="messageName">Optional message name if it differs from type name</param>
+    /// <returns></returns>
+    public static MessageConsumer Register<T>(this MessageConsumer consumer, IMessageHandler<T> handler, string messageName = null) where T: class, IMessage
+    {
+        consumer.RegisterHandler(handler, messageName);
+
+        return consumer;
+    }
+}
